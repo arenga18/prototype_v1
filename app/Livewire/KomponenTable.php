619fc06f40e1cf532fec2104afc70c1ccaf2e6e1
arenga <arengapinnatamarzuki@gmail.com>
2025-05-19 22:26:36 +0700

    update breakdown modul

--- app/Livewire/KomponenTable.php
+++ app/Livewire/KomponenTable.php
@@ -3,82 +3,94 @@
 namespace App\Livewire;
 
 use Livewire\Component;
-use Filament\Tables;
-use Filament\Forms;
-use Filament\Support\Contracts\TranslatableContentDriver;
-use Filament\Tables\Concerns\InteractsWithTable;
-use Filament\Forms\Concerns\InteractsWithForms;
-use Filament\Tables\Contracts\HasTable;
-use Filament\Forms\Contracts\HasForms;
-use Filament\Forms\Components\TextInput;
-use Filament\Forms\Components\Textarea;
-use Filament\Forms\Components\Hidden;
 use App\Models\ModulComponent;
 
-class KomponenTable extends Component implements HasTable, HasForms
+class KomponenTable extends Component
 {
-    use InteractsWithTable, InteractsWithForms;
-
     public $moduls = [];
+    public $groupedComponents = [];
+
+    public $columns = [
+        'komponen',
+        'p',
+        'l',
+        't',
+        'qty',
+        'sub',
+        'jml',
+        'bahan',
+        't_bahan',
+        'l_kedua',
+        'd',
+        't_kedua',
+        'dalam',
+        't_ketiga',
+        'p1',
+        'p2',
+        'l1',
+        'l2',
+        'profile',
+        'rel',
+        'v',
+        'v2',
+        'h',
+        'nama_barang',
+        'panjang',
+        'v_lap',
+        'v_edg',
+        'deskripsi_lapisan',
+        'deskripsi_edging',
+        'engsel',
+        'rel_kedua',
+        'bahan_dasar',
+        'jumlah_anodize',
+        'minifix',
+        'dowel',
+        'jml_siku',
+        'jml_screw'
+    ];
 
     public function mount($moduls)
     {
         $this->moduls = $moduls ?? [];
+        $this->loadGroupedComponents();
     }
 
-    public function makeFilamentTranslatableContentDriver(): ?TranslatableContentDriver
-    {
-        return null;
-    }
-
-    protected function getTableQuery()
+    public function loadGroupedComponents()
     {
         $decodedModuls = array_map(function ($item) {
             return json_decode('"' . $item . '"');
         }, $this->moduls);
 
-        return ModulComponent::query()
-            ->whereIn('modul', $decodedModuls);
+        $components = ModulComponent::whereIn('modul', $decodedModuls)->get();
+
+        $this->groupedComponents = $components->mapWithKeys(function ($modulComponent) {
+            $modul = $modulComponent->modul;
+            $componentList = is_string($modulComponent->component)
+                ? json_decode($modulComponent->component, true)
+                : $modulComponent->component;
+
+
+            return [$modul => $componentList];
+        })->toArray();
     }
 
-    protected function getTableColumns(): array
+    public function updatedGroupedComponents($value, $key)
     {
-        return [
-            Tables\Columns\TextColumn::make('modul')->label('Modul')->sortable()->searchable(),
-            Tables\Columns\TextColumn::make('component')->label('Nama Komponen')->sortable()->searchable(),
-        ];
-    }
+        [$modul, $index, $field] = explode('.', $key);
 
-    protected function getTableActions(): array
-    {
-        return [
-            Tables\Actions\EditAction::make(),
-            Tables\Actions\DeleteAction::make(),
-        ];
-    }
+        $modulComponent = ModulComponent::where('modul', $modul)->first();
 
-    protected function getTableBulkActions(): array
-    {
-        return [
-            Tables\Actions\DeleteBulkAction::make(),
-        ];
-    }
+        if ($modulComponent) {
+            $componentList = is_string($modulComponent->component)
+                ? json_decode($modulComponent->component, true)
+                : $modulComponent->component;
+            $componentList[$index][$field] = $value;
+            $modulComponent->component = json_encode($componentList);
+            $modulComponent->save();
 
-    protected function getTableHeaderActions(): array
-    {
-        return [
-            Tables\Actions\CreateAction::make()
-                ->modalHeading('Tambah Komponen Modul')
-                ->form([
-                    TextInput::make('component')->required(),
-                    Textarea::make('description'),
-                    TextInput::make('quantity')->numeric()->default(1)->minValue(1),
-                    Hidden::make('modul')->default(fn() => is_array($this->moduls[0]) ? ($this->moduls[0]['modul_reference'] ?? null) : $this->moduls[0]),
-                ])
-                ->action(function (array $data): void {
-                    ModulComponent::create($data);
-                }),
-        ];
+            $this->loadGroupedComponents();
+        }
     }
 
     public function render()
